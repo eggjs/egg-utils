9d8b48d57f83c149665c1216a3684cbff60250b1
fengmk2 <fengmk2@gmail.com>
2017-01-13 00:42:46 +0800

    feat: support read framework from package.json
    ```js
    // package.json
    {
      "egg": {
        "framework": "yadan"
      }
    }
    ```
    
    closes https://github.com/eggjs/egg/issues/237#issuecomment-272210081

--- test/getFrameworkOrEggPath.test.js
+++ test/getFrameworkOrEggPath.test.js
@@ -11,6 +11,11 @@ test('get framework dir path success', t => {
   t.is(dirpath, path.join(fixtures, 'aliyun-egg-app/node_modules/aliyun-egg'));
 });
 
+test('get custom framework dir path success when app set app.framework on package.json', t => {
+  const dirpath = utils.getFrameworkOrEggPath(path.join(fixtures, 'yadan-app'));
+  t.is(dirpath, path.join(fixtures, 'yadan-app/node_modules/yadan'));
+});
+
 test('get custom egg dir path success', t => {
   const dirpath = utils.getFrameworkOrEggPath(path.join(fixtures, 'aliyun-egg-app'), [ 'my-old-egg', 'my-new-egg' ]);
   t.is(dirpath, path.join(fixtures, 'aliyun-egg-app/node_modules/my-new-egg'));
