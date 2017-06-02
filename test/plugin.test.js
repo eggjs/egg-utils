5d2ee92020d1068260dc4d1746e6f9eb67ce32b8
Haoliang Gao <sakura9515@gmail.com>
2017-06-02 15:07:41 +0800

    feat: check baseDir and framework (#7)

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -84,5 +84,39 @@ describe('test/plugin.test.js', () => {
         .expect('code', 0)
         .end();
     });
+
+    it('should get plugins when baseDir is not exist', function* () {
+      yield runscript('npminstall', { cwd: tmp });
+
+      const args = JSON.stringify({
+        framework: path.join(tmp, 'node_modules/egg'),
+      });
+      yield coffee.fork(bin, [ args ], { cwd: tmp })
+        .debug()
+        .expect('stdout', /get 11 plugin/)
+        .expect('stdout', /get 1 framework/)
+        .expect('stdout', /get 1 app/)
+        .expect('code', 0)
+        .end();
+    });
+
+    it('should throw when no framework', function* () {
+      yield coffee.fork(bin, [ '{}' ], { cwd: tmp })
+        .debug()
+        .expect('stderr', /framework is required/)
+        .expect('code', 1)
+        .end();
+    });
+
+    it('should throw when framework is not exist', function* () {
+      const args = JSON.stringify({
+        framework: '/noexist',
+      });
+      yield coffee.fork(bin, [ args ], { cwd: tmp })
+        .debug()
+        .expect('stderr', /\/noexist should exist/)
+        .expect('code', 1)
+        .end();
+    });
   });
 });
