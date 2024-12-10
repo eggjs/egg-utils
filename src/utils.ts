5a1d7fa8039ca031f9de750300547783b4e5cffa
fengmk2 <suqian.yf@antgroup.com>
2024-12-10 20:40:29 +0800

    f

--- src/utils.ts
+++ src/utils.ts
@@ -13,6 +13,7 @@ export function getDirname() {
   if (typeof __dirname !== 'undefined') {
     return __dirname;
   }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   return path.dirname(fileURLToPath(import.meta.url));
 }
